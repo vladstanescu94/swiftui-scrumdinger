05dd8b5f342e1a4f575554935562f62caf92281f
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-07 15:52:51 +0200

    Add detail view

---
+++ swiftui-scrumdinger/DetailView.swift
@@ -0,0 +1,46 @@
+//
+//  DetailView.swift
+//  swiftui-scrumdinger
+//
+//  Created by Vlad Stanescu on 07.01.2021.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                    .accessibilityLabel(Text("Start meeting"))
+                HStack {
+                    Label("Length", systemImage: "clock")
+                        .accessibilityLabel(Text("Meeting length"))
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                HStack {
+                    Label("Color", systemImage: "paintpalette")
+                        .accessibilityLabel(Text("Meeting length"))
+                    Spacer()
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(scrum.color)
+                }
+                .accessibilityElement(children: .ignore)
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.data[0])
+        }
+    }
+}
