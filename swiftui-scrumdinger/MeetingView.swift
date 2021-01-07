1ca48fe1c1a860fbb594b928eabd34040479cdba
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-07 14:32:52 +0200

    Add meeting view elements

---
+++ swiftui-scrumdinger/MeetingView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  swiftui-scrumdinger
+//
+//  Created by Vlad Stanescu on 07.01.2021.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}, label: {
+                    Image(systemName: "forward.fill")
+                })
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
--- swiftui-scrumdinger/swiftui_scrumdingerApp.swift
+++ swiftui-scrumdinger/swiftui_scrumdingerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct swiftui_scrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }
