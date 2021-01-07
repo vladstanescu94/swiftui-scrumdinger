b88dde509f8b7259fef6408305b1b2ad067dd1fe
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-07 14:00:53 +0200

    Initial Commit

---
+++ swiftui-scrumdinger/swiftui_scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_scrumdingerApp.swift
+//  swiftui-scrumdinger
+//
+//  Created by Vlad Stanescu on 07.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
