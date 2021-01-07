80d2ac8972455337bc8191ffcd269a30dc9ce77d
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-07 16:39:13 +0200

    Add edit view

---
+++ swiftui-scrumdinger/EditView.swift
@@ -0,0 +1,56 @@
+//
+//  EditView.swift
+//  swiftui-scrumdinger
+//
+//  Created by Vlad Stanescu on 07.01.2021.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    @State private var scrumData: DailyScrum.Data = DailyScrum.Data()
+    @State private var newAttendee = ""
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrumData.title)
+                HStack {
+                    Slider(value: $scrumData.lengthInMinutes, in: 5...30, step: 1.0) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(scrumData.lengthInMinutes)) minutes")
+                }
+                ColorPicker("Color", selection: $scrumData.color)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrumData.attendees, id: \.self) { attendee in
+                    Text(attendee)
+                }
+                .onDelete(perform: { indices in
+                    scrumData.attendees.remove(atOffsets: indices)
+                })
+                HStack {
+                    TextField("New Attendee", text: $newAttendee)
+                    Button(action: {
+                        withAnimation {
+                            scrumData.attendees.append(newAttendee)
+                            newAttendee = ""
+                        }
+                    }, label: {
+                        Image(systemName: "plus.circle.fill")
+                    })
+                    .disabled(newAttendee.isEmpty)
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView()
+    }
+}
