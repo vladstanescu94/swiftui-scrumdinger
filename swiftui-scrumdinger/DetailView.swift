5ea794446e75be5cc8950b505dd352a06dfbae64
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-07 16:00:01 +0200

    Add attendees section

--- swiftui-scrumdinger/DetailView.swift
+++ swiftui-scrumdinger/DetailView.swift
@@ -32,6 +32,13 @@ struct DetailView: View {
                 }
                 .accessibilityElement(children: .ignore)
             }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees, id: \.self) { attendee in
+                    Label(attendee, systemImage: "person")
+                        .accessibilityLabel(Text("Person"))
+                        .accessibilityValue(Text(attendee))
+                }
+            }
         }
         .listStyle(InsetGroupedListStyle())
     }
