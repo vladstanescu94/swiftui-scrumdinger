9ddca544821a4d3faa5d6382644012137444ceb2
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-08 09:43:40 +0200

    Add a way to present the edit view

--- swiftui-scrumdinger/DetailView.swift
+++ swiftui-scrumdinger/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
+    @State private var isPresented = false
     
     var body: some View {
         List {
@@ -44,6 +45,20 @@ struct DetailView: View {
         }
         .listStyle(InsetGroupedListStyle())
         .navigationTitle(scrum.title)
+        .navigationBarItems(trailing: Button("Edit") {
+            isPresented = true
+        })
+        .fullScreenCover(isPresented: $isPresented) {
+            NavigationView {
+                EditView()
+                    .navigationTitle(scrum.title)
+                    .navigationBarItems(leading: Button("Cancel") {
+                        isPresented = false
+                    }, trailing: Button("Done") {
+                        isPresented = false
+                    })
+            }
+        }
     }
 }
 
