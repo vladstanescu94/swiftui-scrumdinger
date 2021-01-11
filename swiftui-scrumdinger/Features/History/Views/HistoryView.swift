8101abe660bec6519acec8ad2fe2754dc932a42a
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-11 14:25:23 +0200

    Add detailed History view

--- swiftui-scrumdinger/Features/History/Views/HistoryView.swift
+++ swiftui-scrumdinger/Features/History/Views/HistoryView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct HistoryView: View {
+    let history: History
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Divider()
+                    .padding(.bottom)
+                Text("Attendees")
+                    .font(.headline)
+                Text(history.attendeeString)
+                if let transcript = history.transcript {
+                    Text("Transcript")
+                        .font(.headline)
+                        .padding(.top)
+                    Text(transcript)
+                }
+            }
+        }
+        .navigationTitle(Text(history.date, style: .date))
+        .padding()
+    }
+}
+
+extension History {
+    var attendeeString: String {
+        ListFormatter.localizedString(byJoining: attendees)
     }
 }
 
 struct HistoryView_Previews: PreviewProvider {
     static var previews: some View {
-        HistoryView()
+        HistoryView(history: History(attendees: ["Jon", "Darla", "Luis"], lengthInMinutes: 10, transcript: "Darla, would you like to start today? Sure, yesterday I reviewed Luis' PR and met with the design team to finalize the UI..."))
     }
 }
