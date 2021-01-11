15ae3d2e827b30397e16cb989483e2d5625c5ec4
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-11 10:48:42 +0200

    Add custom view for meeting timer circle

---
+++ swiftui-scrumdinger/Features/Meeting/Views/MeetingTimerView.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingTimerView.swift
+//  swiftui-scrumdinger
+//
+//  Created by Vlad Stanescu on 11.01.2021.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingTimerView()
+    }
+}
